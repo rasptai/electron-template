774e9ab35925358d13985b26a53512aa53be598c
rasptai <rasptai@gmail.com>
2025-06-30 10:09:32 +0900

    linuxを対象外とするので該当部分を削除

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcMain, shell } from 'electron'
 import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+
+import { electronApp, is, optimizer } from '@electron-toolkit/utils'
 
 function createWindow(): void {
   // Create the browser window.
@@ -10,7 +10,6 @@ function createWindow(): void {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
